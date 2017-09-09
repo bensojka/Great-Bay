956647f1ebb92604af5206fc8a8912e7f459b90f
Brett Leonard <brettleonard86@gmail.com>
2017-09-09 12:51:52 -0500

    connection to mysql

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ uncaught.addListener(function (error) {
 
 var items = [];
 
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "great-bayDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+
 function uno() {
 	inquirer.prompt([
 		{
