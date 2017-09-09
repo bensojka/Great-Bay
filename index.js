c3915cba6c22469381a757359ef8024328859491
Brett Leonard <brettleonard86@gmail.com>
2017-09-09 13:00:38 -0500

    connections

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
 });
 
 function uno() {
